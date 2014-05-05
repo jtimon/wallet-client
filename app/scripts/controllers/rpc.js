a8a0888db4f4fa2f5dd02379c426def9effa6ba3
jtimon <jtimon@monetize.io>
2014-05-05 22:05:00 +0200

    Implement bitcoin rpc view

--- app/scripts/controllers/rpc.js
+++ app/scripts/controllers/rpc.js
@@ -1,10 +1,29 @@
 'use strict';
 
 angular.module('walletClientApp')
-  .controller('RpcCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RpcCtrl', function ($scope, $http) {
+
+    $scope.id = '00001';
+    $scope.method = 'getinfo';
+    $scope.params = '[]';
+
+    $scope.submit = function() {
+
+      $scope.request = {
+        "id": $scope.id,
+        "method": $scope.method,
+        "params": JSON.parse($scope.params)
+      };
+
+      $http.post('http://127.0.0.1:5000/bitcoind', $scope.request)
+        .success( function(data) {
+          // $scope.response = data;
+          $scope.response = JSON.stringify(data, null, 4);
+        })
+        .error( function(data) {
+          $scope.response = JSON.stringify(data, null, 4);
+        });
+
+    };
+
   });
